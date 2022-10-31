8569f79d238d2a7fa2c883d97b1c3f319e6f66c5
kynatov <kynatov001@gmail.com>
2022-10-31 17:01:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    int [] mas = {-4, -2, 2, 3, 6, 8};
+
+    boolean isSorted = false;
+    int buf;
+        while(!isSorted) {
+        isSorted = true;
+        for (int i = 0; i > mas.length-1; i++) {
+            if(mas[i] > mas[i+1]){
+                isSorted = false;
+
+                buf = mas[i];
+                mas[i] = mas[i+1];
+                mas[i+1] = buf;
+            }
+        }
+    }
+        System.out.println(Arrays.toString(mas));
+}
+}
